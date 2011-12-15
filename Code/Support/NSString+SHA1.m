c9e3314b7b9d025b8211c607bd2afbedb940b41d
Fabio Gallonetto <fabio@futureworkshops.com>
2011-12-15 16:43:43 +0000

    Add SHA1 utility extensions to NSString and NSData
    Restkit is providing MD5 support only, but since it's the main library
    we are using for internal projects, it makes sense to add some support
    for other hashing algorithms and expose them on the public interface
    
    Conflicts:
    
    	RestKit.xcodeproj/project.pbxproj

---
+++ Code/Support/NSString+SHA1.m
@@ -0,0 +1,46 @@
+//
+//  NSString+MD5.m
+//  RestKit
+//
+//  Created by Jeff Arena on 4/4/11.
+//  Copyright 2011 Two Toasters
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import <CommonCrypto/CommonDigest.h>
+#import "NSString+SHA1.h"
+#import "../Support/RKFixCategoryBug.h"
+
+RK_FIX_CATEGORY_BUG(NSString_SHA1)
+
+@implementation NSString (SHA1)
+
+- (NSString*)SHA1 {
+    
+    const char *cstr = [self UTF8String];
+    NSData *data = [NSData dataWithBytes:cstr length:self.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return output;
+}
+
+@end
